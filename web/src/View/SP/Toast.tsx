cb47acecc748ae5cd043cdecded6d0d4970ad730
riku <riku@tanide.net>
2021-01-25 03:26:04 +0900

    トーストを表示

--- web/src/View/SP/Toast.tsx
+++ web/src/View/SP/Toast.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { Toast as ToastState } from "../../Types/State";
 import { Observer } from "rxjs";
 import { Event } from "../../Types/Event";
-
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import Alert from "@material-ui/lab/Alert";
+import CancelIcon from "@material-ui/icons/Cancel";
+import UndoIcon from "@material-ui/icons/Undo";
 export const Toast: React.FunctionComponent<{
   toast: ToastState;
   observer: Observer<Event>;
@@ -14,18 +20,27 @@ export const Toast: React.FunctionComponent<{
   const toastType = props.toast.type;
   if (toastType == "edit redo") {
     return (
-      <div>
-        <button onClick={close}>×</button> 保存に失敗しました。
-        <button onClick={redo}>再実行</button>
-      </div>
+      <Snackbar open={true} autoHideDuration={6000} onClose={close}>
+        <Alert severity="success">
+          保存に失敗しました。
+          <button onClick={redo}>再実行</button>
+        </Alert>
+      </Snackbar>
     );
   }
   if (toastType == "edit undo") {
     return (
-      <div>
-        <button onClick={close}>×</button> 変更にしました
-        <button onClick={redo}>取り消し</button>
-      </div>
+      <Snackbar open={true} autoHideDuration={6000} onClose={close}>
+        <Alert severity="success">
+          <IconButton onClick={close}>
+            <CancelIcon />
+          </IconButton>
+          変更しました
+          <IconButton onClick={redo}>
+            <UndoIcon />
+          </IconButton>
+        </Alert>
+      </Snackbar>
     );
   }
   if (toastType == "fetch detail error") {
